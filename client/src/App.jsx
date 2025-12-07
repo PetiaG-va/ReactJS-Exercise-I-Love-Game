8eb208dca43c6428240ff8a6648f0bb6cc56d9a2
Petia Georgieva <petiaG-va@outlook.com>
2025-12-07 17:19:04 +0200

    use unified approach fot controled form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { Route, Routes } from "react-router"
-import Footer from "./components/footer/Footer.jsx"
-import Header from "./components/header/Header.jsx"
-import Home from "./components/home/Home.jsx"
-import Catalog from "./components/catalog/Catalog.jsx"
-import Details from "./components/details/Details.jsx"
-import GameCreate from "./components/game-create/GameCreate.jsx"
-import Register from "./components/register/Register.jsx"
-import { useState } from "react"
-import Login from "./components/login/Login.jsx"
+import { Route, Routes } from "react-router";
+import { useState } from "react";
+
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Home from "./components/home/Home.jsx";
+import Catalog from "./components/catalog/Catalog.jsx";
+import Details from "./components/details/Details.jsx";
+import GameCreate from "./components/game-create/GameCreate.jsx";
+import Register from "./components/register/Register.jsx";
+import Login from "./components/login/Login.jsx";
+import Logout from "./components/logout/Logout.jsx";
+import GameEdit from "./components/game-edit/GameEdit.jsx";
 
 function App() {
 	const [registeredUsers, setRegisteredUsers] = useState([]);
@@ -49,6 +52,7 @@ function App() {
 				<Route path="/games" element={<Catalog />} />
 				<Route path="/games/:gameId/details" element={<Details />} />
 				<Route path="/games/create" element={<GameCreate />} />
+				<Route path="/games/:gameId/edit/" element={<GameEdit />} />
 				<Route path="/register" element={<Register onRegister={registerHandler} />} />
 				<Route path="/login" element={<Login onLogin={loginHandler} />} />
 				<Route path="/logout" element={<Logout onLogout={logoutHandler} />} />
---
+++ client/src/components/game-edit/GameEdit.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+export default function GameEdit() {
+    const initialValues = {
+        title: '',
+        genre: '',
+        players: '',
+        date: '',
+        imageUrl: '',
+        summary: '',
+    };
+
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    return (
+        <section id="edit-page">
+            <form id="add-new-game">
+                <div className="container">
+
+                    <h1>Edit Game</h1>
+
+                    <div className="form-group-half">
+                        <label htmlFor="gameName">Game Name:</label>
+                        <input
+                            type="text"
+                            id="gameName"
+                            name="title"
+                            onChange={changeHandler}
+                            value={values.title}
+                            placeholder="Enter game title..."
+                        />
+                    </div>
+
+                    <div className="form-group-half">
+                        <label htmlFor="genre">Genre:</label>
+                        <input
+                            type="text"
+                            id="genre"
+                            name="genre"
+                            onChange={changeHandler}
+                            value={values.genre}
+                            placeholder="Enter game genre..."
+                        />
+                    </div>
+
+                    <div className="form-group-half">
+                        <label htmlFor="activePlayers">Active Players:</label>
+                        <input
+                            type="number"
+                            id="activePlayers"
+                            name="players"
+                            onChange={changeHandler}
+                            value={values.players}
+                            min="0"
+                            placeholder="0"
+                        />
+                    </div>
+
+                    <div className="form-group-half">
+                        <label htmlFor="releaseDate">Release Date:</label>
+                        <input
+                            type="date"
+                            id="releaseDate"
+                            name="date"
+                            onChange={changeHandler}
+                            value={values.date}
+                        />
+                    </div>
+
+                    <div className="form-group-full">
+                        <label htmlFor="imageUrl">Image URL:</label>
+                        <input
+                            type="text"
+                            id="imageUrl"
+                            name="imageUrl"
+                            onChange={changeHandler}
+                            value={values.imageUrl}
+                            placeholder="Enter image URL..."
+                        />
+                    </div>
+
+                    <div className="form-group-full">
+                        <label htmlFor="summary">Summary:</label>
+                        <textarea
+                            name="summary"
+                            onChange={changeHandler}
+                            value={values.summary}
+                            id="summary"
+                            rows="5"
+                            placeholder="Write a brief summary..."
+                        ></textarea>
+                    </div>
+
+                    <input className="btn submit" type="submit" value="EDIT GAME" />
+                </div>
+            </form>
+        </section>
+    )
+}
